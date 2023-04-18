4546c72d1ead0d2d24797a432deae8c8cc6371b5
Tillanueva <128622581+Tillanueva@users.noreply.github.com>
2023-04-18 15:56:51 -0600

    Update conexion.py

--- Proyecto Python/Project 2 - People Counter/conexion.py
+++ Proyecto Python/Project 2 - People Counter/conexion.py
@@ -3,7 +3,7 @@ import pyodbc
 conn = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-O6UFVI0;DATABASE=PROJECT_PC01;UID=sa;PWD=#Domctrlinf')
 cursor = conn.cursor()
 
-#ahora un query basico
+# ahora un query basico
 cursor.execute("SELECT * FROM conteo ")
 for row in cursor.fetchall():
     print(row)
