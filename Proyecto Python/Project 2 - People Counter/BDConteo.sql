03fd62790f83eb92968f87c5453a4bbca3ed8544
Tillanueva <128622581+Tillanueva@users.noreply.github.com>
2023-05-02 11:28:54 -0600

    Update BDConteo.sql

--- Proyecto Python/Project 2 - People Counter/BDConteo.sql
+++ Proyecto Python/Project 2 - People Counter/BDConteo.sql
@@ -2,12 +2,11 @@ use PROJECT_PC01
 
 CREATE TABLE conteo(
 idconteo int identity primary key,
-fecha varchar(50),
+fecha date,
 entradas int
 );
 
-
-insert into conteo values('2/5/2023', 1)
+insert into conteo values('2023-04-28', 1)
 
 select * from conteo
 
@@ -19,3 +18,19 @@ idconteo int foreign key references conteo(idconteo),
 fechaConteo varchar(50),
 sumConteo int
 );
+
+SELECT fecha, SUM(entradas) AS 'Total Personas'
+FROM conteo
+WHERE fecha = fecha
+group by fecha
+
+SELECT DATENAME(MONTH, DATEADD(MONTH,MONTH(fecha)- 1, '1900-01-01')) Mes, SUM(entradas) AS 'Total Personas'
+FROM conteo
+WHERE MONTH(fecha) =  MONTH(fecha)
+group by MONTH(fecha)
+
+
+SELECT YEAR(fecha) Año, SUM(entradas) As 'Total Personas'
+FROM conteo
+WHERE YEAR(fecha) = YEAR(fecha)
+group by YEAR(fecha)
